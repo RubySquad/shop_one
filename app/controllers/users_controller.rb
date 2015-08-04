28ad7854f6f0f38be0de40e8cb0fa6cfa2afec6a
phagoth <olexandr.averyanov@gmail.com>
2015-08-04 05:56:37 +0300

    autorization mockup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+	def login
+
+		user = find_user_by_login(params[:email])
+		unless user.nil?
+			acces_granted = check_user_password(user, params[:password])
+			render text: "#{acces_granted} - #{user[:name]} #{user[:surname]}"
+		else
+			render text: "No such user"
+		end
+
+	end
+
+private
+
+	def find_user_by_login(login)
+		User.where(email: login).first
+	end
+
+	def check_user_password(user, password)
+		password === user[:password]
+	end
 end
